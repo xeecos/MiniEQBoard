a84ff670431b78cea55e813dbf8458c1ee204e6d
虎子哥 <flashindream@gmail.com>
2023-05-08 20:25:08 +0800

    update

--- src/sensor.cpp
+++ src/sensor.cpp
@@ -13,17 +13,21 @@ void sensor_init()
     compass.init();
     // compass.setCalibration(-2491,0,0,5760,-6746,0);
     compass.setCalibration(-1515, 253, -1072, 857, -1483, 0);
-    compass.setSmoothing(10,true);
+    compass.setSmoothing(10, true);
     mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G);
     mpu.calibrateGyro();
     mpu.setThreshold(3);
+    mpu.setI2CMasterModeEnabled(false);
+    mpu.setI2CBypassEnabled(true);
+    mpu.setSleepEnabled(false);
     // angler.begin(SPI_MISO, SPI_MOSI, SPI_SCK, SPI_CS);
 }
 double sensor_get_azimuth()
 {
     compass.read();
     int az = compass.getAzimuth();
-    LOG_UART("%d,%d,%d\n", compass.getX(), compass.getY(), compass.getZ());
+    // int az = atan2(compass.getY(), compass.getZ()) * 180.0 / M_PI;
+    // LOG_UART("%d,%d,%d\n", compass.getX(), compass.getY(), compass.getZ());
     return az;
 }
 double sensor_get_pitch()
@@ -37,31 +41,35 @@ double sensor_get_pitch()
     {
         norm.XAxis = 65536 - norm.XAxis;
     }
-    double pitch = atan2(norm.YAxis, sqrt(norm.ZAxis * norm.ZAxis + norm.XAxis * norm.XAxis)) * 180.0 / M_PI; 
+    if (norm.YAxis > 32768)
+    {
+        norm.YAxis = 65536 - norm.YAxis;
+    }
+    double pitch = 90 - atan2(norm.YAxis, sqrt(norm.ZAxis * norm.ZAxis + norm.XAxis * norm.XAxis)) * 180.0 / M_PI;
     // LOG_UART("x:%.0f  y:%.0f z:%.0f\n", norm.XAxis, norm.YAxis, norm.ZAxis);
-    return 90.0 - pitch;
+    return pitch;
 }
 double last_angle = 0.0;
 long last_time = 0;
 double last_speed = 0.0;
 double sensor_get_velocity()
 {
-    if(micros()-last_time>10000000)
+    if (micros() - last_time > 10000000)
     {
         float angle = 0.0f;
         angler.getAngleValue(angle);
-        long time = micros()-last_time;
-        if(angle>180)
+        long time = micros() - last_time;
+        if (angle > 180)
         {
             angle -= 360;
         }
-        double speed = (angle-last_angle)/(time/1000000.0);
+        double speed = (angle - last_angle) / (time / 1000000.0);
         last_angle = angle;
         last_time = micros();
-        if(speed<2&&speed>-2)
+        if (speed < 2 && speed > -2)
         {
-            last_speed = last_speed*0.8 + speed*0.2;
-            LOG_UART("%.4f,%.4f,%.4f\n", -1.0/240.0, last_speed, speed);
+            last_speed = last_speed * 0.8 + speed * 0.2;
+            LOG_UART("%.4f,%.4f,%.4f\n", -1.0 / 240.0, last_speed, speed);
         }
         return last_speed;
     }
@@ -70,6 +78,71 @@ double sensor_get_velocity()
 double sensor_get_temperation()
 {
     return mpu.readTemperature();
+}
+
+#ifndef VECTOR_STRUCT_H
+#define VECTOR_STRUCT_H
+struct Vector
+{
+    float XAxis;
+    float YAxis;
+    float ZAxis;
+};
+#endif
+
+float sensor_tilt()
+{
+    compass.read();
+    Vector mag;
+    mag.XAxis = compass.getZ();
+    mag.YAxis = compass.getY();
+    mag.ZAxis = compass.getX();
+    // Vector normAccel = mpu.readScaledAccel();
+
+    // Pitch & Roll
+
+    Vector norm = mpu.readRawAccel();
+    if (norm.ZAxis > 32768)
+    {
+        norm.ZAxis = norm.ZAxis - 65536;
+    }
+    if (norm.XAxis > 32768)
+    {
+        norm.XAxis = norm.XAxis - 65536;
+    }
+    if (norm.YAxis > 32768)
+    {
+        norm.YAxis = norm.YAxis - 65536;
+    }
+    double pitch = atan2(norm.YAxis, norm.ZAxis);//sqrt(norm.ZAxis * norm.ZAxis + norm.XAxis * norm.XAxis));
+    double roll = atan2(norm.XAxis, sqrt(norm.ZAxis * norm.ZAxis + norm.YAxis * norm.YAxis));
+
+    // float v = sqrt((norm.XAxis * norm.XAxis) + (norm.YAxis * norm.YAxis) + (norm.ZAxis * norm.ZAxis)); 
+    // float roll = asin(norm.XAxis / v);
+    // float pitch = asin(norm.YAxis / v);
+
+
+    // pitch = asin(-normAccel.XAxis);
+
+    // if (roll > 0.78 || roll < -0.78 || pitch > 0.78 || pitch < -0.78)
+    // {
+    //     return sensor_get_azimuth();
+    // }
+
+    // Some of these are used twice, so rather than computing them twice in the algorithem we precompute them before hand.
+    float cosRoll = cos(roll);
+    float sinRoll = sin(roll);
+    float cosPitch = cos(pitch);
+    float sinPitch = sin(pitch);
+
+    // Tilt compensation
+    // float Xh = mag.XAxis * cosPitch + mag.ZAxis * sinPitch;
+    // float Yh = mag.XAxis * sinRoll * sinPitch + mag.YAxis * cosRoll - mag.ZAxis * sinRoll * cosPitch;
+    float Xh = mag.XAxis * cosPitch + mag.YAxis * sinRoll * sinPitch - mag.ZAxis * cosRoll * sinPitch;
+    float Yh = mag.YAxis * cosRoll + mag.ZAxis * sinRoll;
+    float heading = atan2(Yh, Xh) * 180.0 / M_PI;
+    printf("heading:%.1f, %.1f %.1f\n", heading, pitch * 180.0 / M_PI,roll * 180.0 / M_PI);
+    return heading;
 }
 int calibrationData[3][2];
 bool changed = false;
--- src/sensor.h
+++ src/sensor.h
@@ -2,6 +2,7 @@
 void sensor_init();
 double sensor_get_azimuth();
 double sensor_get_pitch();
+float sensor_tilt();
 double sensor_get_velocity();
 double sensor_get_temperation();
 void sensor_calibration();

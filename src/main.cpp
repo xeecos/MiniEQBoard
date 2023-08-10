946b5dbcd7cde1f528ba9f8f5c27a118e6e7ff22
xeecos <flashindream@gmail.com>
2023-08-10 17:02:53 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,41 @@
 
 double target_velocity = 1.0/240.0;
 
+double target_alt = 28.226499;
+double current_alt = 0;
+double alt_offset = 0;
+
+void render(void *)
+{
+    while (1)
+    {
+        current_alt = sensor_get_pitch() * 0.3 + current_alt * 0.7;
+        alt_offset = (current_alt - target_alt) * 5;
+        if (alt_offset < -25)
+        {
+            alt_offset = -25;
+        }
+        else if (alt_offset > 25)
+        {
+            alt_offset = 25;
+        }
+        display_clear(true);
+        display_draw_cross(64, 32, 127, 64, false);
+        display_draw_cross(64, 32 + alt_offset, 10, 10, false);
+        char * str = (char*)malloc(16);
+        sprintf(str,"ALT:%.0f\0",current_alt);
+        display_draw_text(8, 0, str, false);
+        free(str);
+        display_render();
+        delay(1);
+    }
+}
 void setup()
 {
     LOG_INIT(115200);
-    // sensor_init();
-    // display_init();
+    sensor_init();
+    display_init();
+    xTaskCreatePinnedToCore(render, "render", 8192, NULL, 10, NULL, 1);
     stepper_init();
     // stepper_runSpeed(target_velocity);
     // service_init();
